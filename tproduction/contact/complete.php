c48f2421d059563fc8da208376ae556e37fe1d8f
Shoma Naruse <shoma_nrunru@icloud.com>
2024-10-11 02:18:03 +0900

    update

---
+++ tproduction/contact/complete.php
@@ -0,0 +1,65 @@
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = $_POST["name"];
+        $hurigana = $_POST["hurigana"];
+        $company = $_POST["company"];
+        $tel = $_POST["tel"];
+        $mail = $_POST["mail"];
+        $title = $_POST["title"];
+        $message = $_POST["inquiry"];
+        $to = "shoma_nrunru@icloud.com";
+        $subject = "You got a message from contact form.";//日本語文字化け要対応
+        $headers = "MIME-Version: 1.0\r\n"
+         . "Content-Transfer-Encoding: 8bit\r\n"
+         . "Content-Type: text/plain; charset=UTF-8\r\n"
+         . "From: your_email@example.com";
+        $body = "名前: $name\nふりがな: $hurigana\n社名: $company\n電話番号: $tel\nメールアドレス: $mail\n件名: $title\n\n$message";
+        if (mail($to, $subject, $body, $headers)) {
+            $message1 = "お問い合わせが完了しました";
+            $message2 = "お問い合わせいただき、ありがとうございます。";
+            $message3 = "お問い合わせいただいた内容を確認の上、後日ご連絡させていただきます。";
+        } else {
+            $message1 = "エラーが発生しました。";
+            $message2 = "お手数ですが、もう一度お試しください。";
+            $message3 = "";
+        }
+    } else {
+        header("Location: contact.php");
+    }
+?>
+
+<html>
+  <head>
+  <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/complete.css">
+    <link rel="icon" href="../../images/logo/sezan2.ico">
+    <title>送信完了 | 聖傘</title>
+  </head>
+  <body>
+    <div id="header"></div>
+    <div class="h1-title">
+        <h1>送信完了</h1>
+    </div>
+    <div class="contents">
+      <h2><?php echo $message1; ?></h2>
+
+      <h3><?php echo $message2; ?></h3>
+      <h3><?php echo $message3; ?></h3>
+
+      <p><a href="contact.php">→ お問い合わせフォームに戻る</a></p>
+    </div>
+
+    <div id="footer"></div>
+
+    <script>
+        fetch("../component/header/header.html")
+            .then((response) => response.text())
+            .then((data) => document.querySelector("#header").innerHTML = data);
+        fetch("../component/footer/footer.html")
+            .then((response) => response.text())
+            .then((data) => document.querySelector("#footer").innerHTML = data);
+    </script>
+
+  </body>
+</html>
---
+++ tproduction/contact/confirm.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/confirm.css">
+    <link rel="icon" href="../../images/logo/sezan2.ico">
+    <title>確認 | 聖傘</title>
+</head>
+
+<body>
+
+    <div class="h1-title">
+        <h1>確認</h1>
+    </div>
+
+    <form action="complete.php" method="post">
+
+        <h4>以下の内容で送信してよろしいですか？</h4>
+
+        <div class="section">
+            <h2>お名前</h2>
+            <h3><?php echo $_POST["name"]; ?></h3>
+        </div>
+
+        <div class="section">
+            <h2>ふりがな</h2>
+            <h3><?php echo $_POST["hurigana"]; ?></h3>
+        </div>
+
+        <div class="section">
+            <h2>会社名</h2>
+            <h3><?php echo $_POST["company"]; ?></h3>
+        </div>
+
+        <div class="section">
+            <h2>電話番号</h2>
+            <h3><?php echo $_POST["tel"]; ?></h3>
+        </div>
+
+        <div class="section">
+            <h2>メールアドレス</h2>
+            <h3><?php echo $_POST["mail"]; ?></h3>
+        </div>
+
+        <div class="section">
+            <h2>件名</h2>
+            <h3><?php echo $_POST["title"]; ?></h3>
+        </div>
+
+        <div class="section">
+            <h2>お問い合わせ内容</h2>
+            <h3><?php echo $_POST["inquiry"]; ?></h3>
+        </div>
+
+        <input type="hidden" name="name" value="<?php echo $_POST["name"]; ?>">
+        <input type="hidden" name="hurigana" value="<?php echo $_POST["hurigana"]; ?>">
+        <input type="hidden" name="company" value="<?php echo $_POST["company"]; ?>">
+        <input type="hidden" name="tel" value="<?php echo $_POST["tel"]; ?>">
+        <input type="hidden" name="mail" value="<?php echo $_POST["mail"]; ?>">
+        <input type="hidden" name="title" value="<?php echo $_POST["title"]; ?>">
+        <input type="hidden" name="inquiry" value="<?php echo $_POST["inquiry"]; ?>">
+
+        <div class="buttons">
+            <button class="button back" type="button" onclick="history.back()">戻る</button>
+            <input class="button submit" type="submit" value="送信" onclick="">
+        </div>
+
+    </form>
+
+</body>
+</html>
