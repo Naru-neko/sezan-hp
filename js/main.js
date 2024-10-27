d90f264a4168e2d4d032f4a8addc1fb480d4f193
Shoma Naruse <shoma_nrunru@icloud.com>
2024-10-27 18:08:20 +0900

    update

--- js/main.js
+++ js/main.js
@@ -1,139 +1,169 @@
-const cproContent = document.getElementById("cpro-content");
-const agContent = document.getElementById("ag-content");
-
-document.querySelector('#cpro-title').addEventListener('mouseover', function() {
-    document.querySelector('#logo').src = 'images/logo/sezan1-black.png';
-    document.querySelector('#logo').animate(
-        [ { width: '32%' }, {  width: '15%' } ],
-        { duration: 500, fill: 'forwards' }
+function opener() {
+    document.querySelector(".up-left").animate(
+        [ { transform: ' translateY(0%)' }, { transform: ' translateY(-100%)' } ],
+        { duration: 700, fill: 'forwards' ,delay: 1000}
     );
-    document.querySelector('#logo').animate(
-        [ { transform: 'translate(-50%, -50%)' }, { transform: 'translate(0, 0)' } ],
-        { duration: 500, fill: 'forwards' }
-    );
-    document.querySelector('#logo').animate(
-        [ { top: '50%' }, { top: '2%' } ],
-        { duration: 500, fill: 'forwards' }
-    );
-    document.querySelector('#logo').animate(
-        [ { left: '50%' }, { left: '78%' } ],
-        { duration: 500, fill: 'forwards' }
-    );
-    document.querySelector('.agent').animate(
-        [ { width: '50vw' }, { width: 0 } ],
-        { duration: 500, fill: 'forwards' }
-    );
-    document.querySelector('#cpro-contents').animate(
-        [ { opacity: 0 }, { opacity: 1 } ],
-        { duration: 500, fill: 'forwards', delay: 300 }
-    );
-    document.querySelector('#cpro-contents').animate(
-        [ { display: 'none' }, { display: 'flex' } ],
-        { duration: 500, fill: 'forwards'}
-    );
-});
-
-document.querySelector('#cpro-title').addEventListener('mouseleave', function() {
-    document.querySelector('#logo').src = 'images/logo/sezan1-rev.png';
-    document.querySelector('#logo').animate(
-        [ { width: '15%' }, {  width: '32%' } ],
-        { duration: 500, fill: 'forwards' }
-    );
-    document.querySelector('#logo').animate(
-        [ { transform: 'translate(0%, 0%)' }, { transform: 'translate(-50%, -50%)' } ],
-        { duration: 500, fill: 'forwards' }
-    );
-    document.querySelector('#logo').animate(
-        [ { top: '2%' }, { top: '50%' } ],
-        { duration: 500, fill: 'forwards' }
-    );
-    document.querySelector('#logo').animate(
-        [ { left: '78%' }, { left: '50%' } ],
-        { duration: 500, fill: 'forwards' }
-    );
-    document.querySelector('.agent').animate(
-        [ { width: 0 }, { width: '50vw' } ],
-        { duration: 500, fill: 'forwards' }
-    );
-    document.querySelector('#cpro-contents').animate(
+    document.querySelector(".up-left").animate(
         [ { opacity: 1 }, { opacity: 0 } ],
-        { duration: 200, fill: 'forwards'}
+        { duration: 700, fill: 'forwards' ,delay: 1000}
     );
-    document.querySelector('#cpro-contents').animate(
-        [ { display: 'flex' }, { display: 'none' } ],
-        { duration: 500, fill: 'forwards'}
+    document.querySelector(".down-right").animate(
+        [ { transform: ' translateY(0%)' }, { transform: ' translateY(100%)' } ],
+        { duration: 700, fill: 'forwards' ,delay: 1000}
     );
-});
-
-
-document.querySelector('#ag-title').addEventListener('mouseover', function() {
-    document.querySelector('#logo').src = 'images/logo/sezan1-white.png';
-    document.querySelector('#logo').animate(
-        [ { width: '32%' }, {  width: '15%' } ],
-        { duration: 500, fill: 'forwards' }
-    );
-    document.querySelector('#logo').animate(
-        [ { transform: 'translate(-50%, -50%)' }, { transform: 'translate(0, 0)' } ],
-        { duration: 500, fill: 'forwards' }
-    );
-    document.querySelector('#logo').animate(
-        [ { top: '50%' }, { top: '2%' } ],
-        { duration: 500, fill: 'forwards' }
-    );
-    document.querySelector('#logo').animate(
-        [ { left: '50%' }, { left: '2%' } ],
-        { duration: 500, fill: 'forwards' }
-    );
-    document.querySelector('.cproduction').animate(
-        [ { width: '50vw' }, { width: 0 } ],
-        { duration: 500, fill: 'forwards' }
-    );
-    document.querySelector('main').animate(
-        [ { backgroundColor: 'white' }, { backgroundColor: 'black' } ],
-        { duration: 1, fill: 'forwards'}
-    );
-    document.querySelector('#ag-contents').animate(
-        [ { opacity: 0 }, { opacity: 1 } ],
-        { duration: 500, fill: 'forwards', delay: 300 }
-    );
-    document.querySelector('#ag-contents').animate(
-        [ { display: 'none' }, { display: 'flex' } ],
-        { duration: 500, fill: 'forwards'}
-    );
-});
-
-document.querySelector('#ag-title').addEventListener('mouseleave', function() {
-    document.querySelector('#logo').src = 'images/logo/sezan1-rev.png';
-    document.querySelector('#logo').animate(
-        [ { width: '15%' }, {  width: '32%' } ],
-        { duration: 500, fill: 'forwards' }
-    );
-    document.querySelector('#logo').animate(
-        [ { transform: 'translate(0%, 0%)' }, { transform: 'translate(-50%, -50%)' } ],
-        { duration: 500, fill: 'forwards' }
-    );
-    document.querySelector('#logo').animate(
-        [ { top: '2%' }, { top: '50%' } ],
-        { duration: 500, fill: 'forwards' }
-    );
-    document.querySelector('#logo').animate(
-        [ { left: '2%' }, { left: '50%' } ],
-        { duration: 500, fill: 'forwards' }
-    );
-    document.querySelector('.cproduction').animate(
-        [ { width: 0 }, { width: '50vw' } ],
-        { duration: 500, fill: 'forwards' }
-    );
-    document.querySelector('main').animate(
-        [ { backgroundColor: 'black' }, { backgroundColor: 'white' } ],
-        { duration: 1, fill: 'forwards' , delay: 500}
-    );
-    document.querySelector('#ag-contents').animate(
+    document.querySelector(".down-right").animate(
         [ { opacity: 1 }, { opacity: 0 } ],
-        { duration: 200, fill: 'forwards'}
+        { duration: 700, fill: 'forwards' ,delay: 1000}
     );
-    document.querySelector('#ag-contents').animate(
-        [ { display: 'flex' }, { display: 'none' } ],
-        { duration: 500, fill: 'forwards' }
+    setTimeout(() => {
+        document.querySelector(".up-left").style.display = 'none';
+        document.querySelector(".down-right").style.display = 'none';
+    }, 1700);
+}
+
+function closer() {
+    document.querySelector(".up-left").style.display = 'block';
+    document.querySelector(".down-right").style.display = 'block';
+    document.querySelector(".up-left").animate(
+        [ { transform: ' translateY(-100%)' }, { transform: ' translateY(0%)' } ],
+        { duration: 700, fill: 'forwards'}
     );
+    document.querySelector(".up-left").animate(
+        [ { opacity: 0 }, { opacity: 1 } ],
+        { duration: 700, fill: 'forwards'}
+    );
+    document.querySelector(".down-right").animate(
+        [ { transform: ' translateY(100%)' }, { transform: ' translateY(0%)' } ],
+        { duration: 700, fill: 'forwards'}
+    );
+    document.querySelector(".down-right").animate(
+        [ { opacity: 0 }, { opacity: 1 } ],
+        { duration: 700, fill: 'forwards'}
+    );
+}
+
+function lineOpen(which) {
+    document.querySelector(which + " .humburger-line:nth-child(1)").animate(
+        [ { rotate: '0deg' }, { rotate: '-765deg' } ],
+        { duration: 700, fill: 'forwards' ,delay: 1000}
+    );
+    document.querySelector(which + " .humburger-line:nth-child(2)").animate(
+        [ { rotate: '0deg' }, { rotate: '-765deg' } ],
+        { duration: 700, fill: 'forwards', delay: 1000}
+    );
+}
+
+function lineClose(which) {
+    document.querySelector(which + " .humburger-line:nth-child(1)").animate(
+        [ { rotate: '-765deg' }, { rotate: '0deg' } ],
+        { duration: 700, fill: 'forwards' }
+    );
+    document.querySelector(which + " .humburger-line:nth-child(2)").animate(
+        [ { rotate: '-765deg' }, { rotate: '0deg' } ],
+        { duration: 700, fill: 'forwards' }
+    );
+}
+
+document.querySelector("#drawer-input").addEventListener("click", function() {
+    const mediaQuery = window.matchMedia('(max-width: 1250px)')
+    if (document.querySelector("#drawer-input").checked) {
+        document.querySelector(".tpro-content").style.display = "none";
+
+        document.querySelector(".tpro").animate(
+            [{ opacity: 1 }, { opacity: 0 }],
+            { duration: 200, fill: 'forwards'}
+        );
+        document.querySelector(".touch").animate(
+            [{ opacity: 1 }, { opacity: 0 }],
+            { duration: 200, fill: 'forwards'}
+        );
+        document.querySelector(".logo").animate(
+            [{ opacity: 1 }, { opacity: 0 }],
+            { duration: 700, fill: 'forwards', delay: 300}
+        );
+        opener();
+        lineOpen('.cpro');
+        setTimeout(() => {
+            document.querySelector(".tpro").style.display = "none";
+            document.querySelector(".touch").style.display = "none";
+            document.querySelector(".logo").style.display = "none";
+        }, 1000);
+    } else {
+        document.querySelector(".tpro").style.display = "block";
+        document.querySelector(".touch").style.display = "flex";
+        document.querySelector(".logo").style.display = "block";
+        document.querySelector(".tpro").animate(
+            [{ opacity: 0 }, { opacity: 1 }],
+            { duration: 200, fill: 'forwards' ,delay: 700}
+        );
+        document.querySelector(".touch").animate(
+            [{ opacity: 0 }, { opacity: 1 }],
+            { duration: 200, fill: 'forwards' ,delay: 700}
+        );
+        document.querySelector(".logo").animate(
+            [{ opacity: 0 }, { opacity: 1 }],
+            { duration: 300, fill: 'forwards', delay: 900}
+        );
+        setTimeout(() => {
+            if (mediaQuery.matches) {
+                document.querySelector(".tpro-content").style.display = "flex";
+            } else {
+                document.querySelector(".tpro-content").style.display = "block";
+            }
+        }, 1000);
+        lineClose('.cpro');
+        closer();
+    }
+});
+
+document.querySelector("#drawer-input2").addEventListener("click", function() {
+    const mediaQuery = window.matchMedia('(max-width: 1250px)')
+    if (document.querySelector("#drawer-input2").checked) {
+        document.querySelector(".cpro-content").style.display = "none";
+
+        document.querySelector(".cpro").animate(
+            [{ opacity: 1 }, { opacity: 0 }],
+            { duration: 200, fill: 'forwards'}
+        );
+        document.querySelector(".tpro .touch").animate(
+            [{ opacity: 1 }, { opacity: 0 }],
+            { duration: 200, fill: 'forwards'}
+        );
+        document.querySelector(".logo").animate(
+            [{ opacity: 1 }, { opacity: 0 }],
+            { duration: 700, fill: 'forwards', delay: 300}
+        );
+        opener();
+        lineOpen('.tpro');
+        setTimeout(() => {
+            document.querySelector(".cpro").style.display = "none";
+            document.querySelector(".tpro .touch").style.display = "none";
+            document.querySelector(".logo").style.display = "none";
+        }, 1000);
+    } else {
+        document.querySelector(".cpro").style.display = "block";
+        document.querySelector(".tpro .touch").style.display = "flex";
+        document.querySelector(".logo").style.display = "block";
+        document.querySelector(".cpro").animate(
+            [{ opacity: 0 }, { opacity: 1 }],
+            { duration: 200, fill: 'forwards' ,delay: 700}
+        );
+        document.querySelector(".tpro .touch").animate(
+            [{ opacity: 0 }, { opacity: 1 }],
+            { duration: 200, fill: 'forwards' ,delay: 700}
+        );
+        document.querySelector(".logo").animate(
+            [{ opacity: 0 }, { opacity: 1 }],
+            { duration: 300, fill: 'forwards', delay: 900}
+        );
+        setTimeout(() => {
+            if (mediaQuery.matches) {
+                document.querySelector(".cpro-content").style.display = "flex";
+            } else {
+                document.querySelector(".cpro-content").style.display = "block";
+            }
+        }, 1000);
+        lineClose('.tpro');
+        closer();
+    }
 });
