bdf7c98f382fb8c7b41211847213a9173b6ad41a
Shoma Naruse <shoma_nrunru@icloud.com>
2024-11-20 10:13:47 +0900

    update

--- tproduction/component/paging/paging.js
+++ tproduction/component/paging/paging.js
@@ -7,17 +7,3 @@ window.onload = function() {
         document.querySelector('.paging-in').style.display = 'none';
     }, 800);
 }
-
-// function go(e) {
-//     var url = e.getAttribute('href');
-//     console.log(e);
-//     document.querySelector('.paging').style.display = 'block';
-//     document.querySelector('.paging').animate(
-//         [ { transform: 'rotateY(90deg)' }, { transform: 'rotateY(0deg)' } ],
-//         { duration: 500, fill: 'forwards', easing: 'ease-in' }
-//     );
-//     setTimeout(() => {
-//         window.location.href = url;
-//     }, 600);
-//     return false;
-// }
