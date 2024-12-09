95cfe3ceb4c488a54b1b2e27333d0cc9bcdc0481
Shoma Naruse <shoma_nrunru@icloud.com>
2024-12-09 17:11:14 +0900

    update

--- cproduction/contact/contact.php
+++ cproduction/contact/contact.php
@@ -3,76 +3,81 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Cache-Control" content="no-store">
+    <link rel="stylesheet" href="css/contact.css">
     <link rel="stylesheet" href="/cproduction/cproglobal.css">
     <link rel="stylesheet" href="/cproduction/component/paging/paging.css">
     <script src="/cproduction/component/paging/paging.js"></script>
     <link rel="icon" href="../../images/logo/sezan2.ico">
-    <title>CONTACT | 聖傘</title>
+    <title>CONTACT | 聖傘-CreativePRODUCTION</title>
 </head>
 
 <body>
     <div class="paging"></div>
     <div class="paging-in"></div>
 
-    <main>
-        <div class="h1-title">
-            <h1>CONTACT</h1>
-        </div>
-        <h3 id="info">制作に関するお問い合わせ、その他お問い合わせは<br>
-        下記メールアドレスまでご連絡ください。
+    <div id="humburger"></div>
+    <div class="h1-title">
+        <h1>CONTACT</h1>
+        <img src="../../images/mail.png" alt="">
+    </div>
+
+    <form action="confirm.php" method="post">
+
+        <h3 id="info">制作や関するお問い合わせ、<br>
+            その他お問い合わせは下記のフォームよりご連絡ください。
         </h3>
-        <h2>contact2@sezan.tokyo</h2>
-    </main>
+
+        <div class="section name">
+            <div class="raw-name">
+                <h2>お名前<span class="required">必須</span></h2>
+                <input type="text" name="name" placeholder="名前を入力" required>
+            </div>
+            <div class="hurigana">
+                <h2>ふりがな<span class="required">必須</span></h2>
+                <input type="text" name="hurigana" placeholder="ふりがなを入力" required>
+            </div>
+        </div>
+
+        <div class="section company">
+            <h2>貴社名</h2>
+            <input type="text" name="company" placeholder="貴社名を入力">
+        </div>
+
+        <div class="section tel">
+            <h2>電話番号<span class="required">必須</span></h2>
+            <input type="text" name="tel" placeholder="電話番号を入力" required>
+        </div>
+
+        <div class="section mail">
+            <h2>メールアドレス<span class="required">必須</span></h2>
+            <input type="text" name="mail" placeholder="メールアドレスを入力" required>
+        </div>
+
+        <div class="section title">
+            <h2>件名<span class="required">必須</span></h2>
+            <input type="text" name="title" placeholder="件名を入力" required>
+        </div>
+
+        <div class="section content">
+            <h2>お問い合わせ内容<span class="required">必須</span></h2>
+            <textarea name="inquiry" id="contact-content" cols="30" rows="8" placeholder="内容を入力" required></textarea>
+        </div>
+
+        <input type="submit" id="submit" value="入力内容の確認" onclick="">
+
+    </form>
 
     <div id="footer"></div>
 
     <script>
+        fetch("../component/header/humburger.html")
+            .then((response) => response.text())
+            .then((data) => document.querySelector("#humburger").innerHTML = data);
         fetch("../component/footer/footer.html")
             .then((response) => response.text())
             .then((data) => document.querySelector("#footer").innerHTML = data);
     </script>
-
-    <style>
-        html {
-            margin: 0;
-            padding: 0;
-            width: 100%;
-        }
-
-        body{
-            background-color: #f0f0f0;
-            width: auto;
-            margin: 0;
-            padding: 0;
-            color: rgb(31, 31, 31);
-            font-weight: 300;
-        }
-
-        main{
-            margin: 0;
-            padding: 0;
-            width: 100%;
-            height: 100vh;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-        }
-
-        h1 {
-            font-size: 11vmin;
-            color: #aaaaaa;
-        }
-
-        h2 {
-            font-size: 5vmin;
-            text-decoration: underline;
-        }
-
-        @media screen and (max-width:1250px) {
-
-        }
-    </style>
 
 </body>
 </html>
